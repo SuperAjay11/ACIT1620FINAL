e52a13d262289fee735c238b1dce5efd6ba47cf1
SuperAjay11 <91698588+SuperAjay11@users.noreply.github.com>
2021-12-10 13:11:58 -0800

    SubmitButtonAdded

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-//declare 
+//declare global variables for script
 let body = document.querySelector('body');
 let DarkMode = document.querySelector("input.toggle");
 
@@ -11,4 +11,27 @@ DarkMode.addEventListener("click", (e) => {
     body.classList.toggle("light-theme", true);
     body.classList.toggle("dark-theme", false);
     }
+});
+//help of my assignment part B made this part pretty easy for me 
+let button = document.querySelector('button#frm-btn');
+let message = document.querySelector('textarea#msg');
+let personName = document.querySelector('input#name');
+let email = document.querySelector('input#email');
+let service = 
+{
+  email: "",
+  name: "",
+  service: "Some service, facial transmogrification",
+  message: "",
+};
+//added evevnt listener for button
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    service['email'] = email.value;
+    service['name'] = personName.value;
+    service['message'] = message.value;
+    email.value = "";
+    personName.value = "";
+    message.value = "";
+    console.log(service);
 });
