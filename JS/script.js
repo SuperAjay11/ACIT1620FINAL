d1dd35c532b82c10049a3f1d61f34aea9e514b58
SuperAjay11 <91698588+SuperAjay11@users.noreply.github.com>
2021-12-10 13:04:04 -0800

    DarkModeAdded

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,14 @@
-toggle = document.querySelector(".toggle")
+//declare 
+let body = document.querySelector('body');
+let DarkMode = document.querySelector("input.toggle");
 
-toggle.addEventListener("click", changeTheme)
-
-function changeTheme(event) {
-    listelement = document.getElementsByTagName('*')
-    for (let i = 0; i < listelement.length; i++) {                      //perfectly changes default light theme to dark theme by the click of the button
-        classesList = listelement[i].classList
-        if (!classesList.contains('dark-theme')) {
-            classesList.add('dark-theme')
-            classesList.remove('light-theme')
-        } 
-        else {
-            classesList.remove('dark-theme')
-            classesList.add('light-theme')
-        }
+DarkMode.addEventListener("click", (e) => {
+    if (DarkMode.checked == true){
+        body.classList.toggle("dark-theme", true);
+        body.classList.toggle("light-theme", false);
+      } 
+    else {
+    body.classList.toggle("light-theme", true);
+    body.classList.toggle("dark-theme", false);
     }
-
-}
+});
